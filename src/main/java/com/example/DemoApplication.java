65820e950c2db2b7a500c4f8545611acd292d738
Yoaz Menda <yoazmenda@gmail.com>
2016-04-15 12:31:57 +0300

    Added controller

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -8,6 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Spring Application!");
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args); 
 	}
 }
---
+++ src/main/java/com/example/web/rest/HelloController.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello from server";
+    }
+    
+
+}
