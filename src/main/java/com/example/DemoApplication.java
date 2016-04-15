cc29ee15317ef092054e2c37db0aba842a20704f
Yoaz Menda <yoazmenda@gmail.com>
2016-04-15 11:49:08 +0300

    Added Build and run tasks

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.Out.Println("Hello from Spring Application!");
+		System.out.println("Hello from Spring Application!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
